bad4337863e955704cd61d3df2e84747787bd73d
issey-xeudo <tair38613@gmail.com>
2025-05-13 20:40:08 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+﻿from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # Данные, которые мы будем передавать в шаблон
+    title = "Мой сайт"
+    description = "Это сайт с динамическим контентом!"
+    return render_template('index.html', title=title, description=description)
+
+if __name__ == '__main__':
+    app.run(host='192.168.100.16', port=5000, debug=True)
